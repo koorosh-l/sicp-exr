f4519162c450a57016252b735b659b173d439d4e
koorosh <koorosh.akl@protonmail.com>
2022-11-15 12:41:07 +0300

    prime.scm now has sieve impelemented

--- prime.scm
+++ prime.scm
@@ -11,35 +11,33 @@
 	    (euclidean-quotient a mem))]))
   (iter 2 a))
 
-(define max (read))
-(define-inlinable (index->num i) (+ i 2))
-(define-inlinable (number->index n) (- n 2))
-
 (define (eliminate! i arr)
-  (let loop ([m (index->num i)] [l (bitvector-length arr)])
-    ;;(display i) (newline);; (display m) (newline) 
+  (let loop ([m (+ i i)]
+	     [ub (bitvector-length arr)])
     (cond
-     [(>= m l) #t]
+     [(>= m ub) #t]
      [else (bitvector-set! arr m #f)
-	   (loop (+ m i) l)])))
-(define (sive max)
-  (define size (1- max))
+	   (loop (+ m i) ub)])))
+
+(define (sieve max)
+  (define size max)
   (define arr (make-bitvector size #t))
+  ;; (bitvector-set! arr 0 #f)
+  ;; (bitvector-set! arr 1 #f)
   (let loop ([ub (1- size)]
-	     [i 0])
+	     [i 2])
     (cond
      [(>= i ub) arr]
      [else (eliminate! i arr)
-	   (loop (1+ (quotient size (+ i 2)))
+	   (loop ub;;(1+ (quotient size (+ i 2)))
 		 (1+ i))])))
 
-(let loop ([i 0])
-  (cond
-   [(= i size) #t]
-   [else (when (bitvector-ref arr i)
-	   (display (+ i 2))
-	   (newline))
-	 (loop (1+ i))]))
+(define (sieve-out->numlist arr)
+  (let loop ([i 0] [size (bitvector-length arr)])
+    (cond
+     [(>= i size) '()]
+     [(bitvector-ref arr i) (cons i (loop (1+ i) size))]
+     [else (loop (1+ i) size)])))
 (define (check arr)
   (let loop ([l (bitvector-length arr)]
 	     [i 0])
